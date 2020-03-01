7267cf4a98cb4eb4d3b1a8c89fff49db5e05ce0e
blupine <skwint11@gmail.com>
2020-03-01 18:15:30 +0900

    implement CanvasActivity for simple drawing

--- app/src/main/java/com/example/drawingproject/CanvasActivity.java
+++ app/src/main/java/com/example/drawingproject/CanvasActivity.java
@@ -1,14 +1,32 @@
 package com.example.drawingproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+
+import com.example.drawingproject.CanvasView.CanvasView;
 
 public class CanvasActivity extends AppCompatActivity {
 
+    private CanvasView mCanvas;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
+        mCanvas = findViewById(R.id.canvas);
+
     }
+    public CanvasView getCanvas() {
+        return this.mCanvas;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.canvas, menu);
+        return true;
+    }
+
 }
